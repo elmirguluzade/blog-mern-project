3f91857726b539c40c350147ef67abd0af6ab374
Elmir <elmir.quluzade627@gmail.com>
2023-03-13 10:40:56 +0400

    fixed

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,52 +1,75 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect, useRef, useContext } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios'
-import './Login.css'
-import { userContext } from '../../Context'
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import axios from "axios";
+import "./Login.css";
+import { userContext } from "../../Context";
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [data, setData] = useState('')
-  const [fetch, setFetch] = useState(false)
-  const didMount = useRef(false)
-  const { setUserInfo } = useContext(userContext)
-  const navigate = useNavigate()
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [data, setData] = useState("");
+  const [fetch, setFetch] = useState(false);
+  const [passVisible, setPassVisible] = useState(false);
+  const didMount = useRef(false);
+  const { setUserInfo } = useContext(userContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (didMount.current) {
-      axios('http://localhost:4000/user/login', {
+      axios("http://localhost:4000/user/login", {
         method: "post",
         data,
-        withCredentials: true
+        withCredentials: true,
       })
         .then((response) => response.data)
         .then((data) => {
-          localStorage.setItem('name', data.user.name)
-          setUserInfo(data.user._id)
-          toast.success('Logged In', { position: "top-right", autoClose: 500, hideProgressBar: true, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, theme: "light", });
+          localStorage.setItem("name", data.user.name);
+          setUserInfo(data.user._id);
+          toast.success("Logged In", {
+            position: "top-right",
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
           setTimeout(() => {
-            navigate('/')
-          }, 500)
+            navigate("/");
+          }, 500);
         })
-        .catch(err => {
-          if (err.response.status === 401 || err.response.message === "Email or password is incorrect") {
-            toast.error('Wrong credentials', { position: "top-right", autoClose: 500, hideProgressBar: true, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, theme: "light", });
+        .catch((err) => {
+          if (
+            err.response.status === 401 ||
+            err.response.message === "Email or password is incorrect"
+          ) {
+            toast.error("Wrong credentials", {
+              position: "top-right",
+              autoClose: 500,
+              hideProgressBar: true,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
           }
-        })
+        });
     } else {
-      didMount.current = true
+      didMount.current = true;
     }
-  }, [fetch])
+  }, [fetch]);
 
   const submitForm = (e) => {
     e.preventDefault();
-    setData({ email, password })
+    setData({ email, password });
     if (!email || !password) {
-      toast.error('Please enter all information', {
+      toast.error("Please enter all information", {
         position: "top-right",
         autoClose: 1000,
         hideProgressBar: true,
@@ -56,44 +79,60 @@ const Login = () => {
         progress: undefined,
         theme: "light",
       });
-      return
+      return;
     }
-    setFetch(!fetch)
-  }
+    setFetch(!fetch);
+  };
 
   return (
-    <main className='loginContainer'>
+    <main className="loginContainer">
       <div className="login">
         <h2>Login</h2>
         <form onSubmit={(e) => submitForm(e)}>
           <div className="input">
             <input
               type="text"
-              id='email'
-              className='text'
+              id="email"
+              className="text"
               value={email}
-              onChange={e => setEmail(e.target.value)}
-              placeholder=' ' />
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder=" "
+            />
             <label htmlFor="email">Email</label>
           </div>
           <div className="input">
             <input
-              type="password"
-              id='password'
-              className='text'
+              id="password"
+              type={!passVisible ? "password" : "text"}
+              className="text"
               value={password}
-              onChange={e => setPassword(e.target.value)}
-              placeholder=' ' />
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder=" "
+            />
             <label htmlFor="password">Password</label>
+            <p className="icon" onClick={() => setPassVisible(!passVisible)}>
+              {!passVisible ? <FaEye /> : <FaEyeSlash />}
+            </p>
           </div>
-          <div className="forget"><NavLink to='/forget' className="forgetPass">Forget password?</NavLink></div>
-          <button type="submit" className='submitBtn'>Login</button>
+          <div className="forget">
+            <NavLink to="/forget" className="forgetPass">
+              Forget password?
+            </NavLink>
+          </div>
+          <button type="submit" className="submitBtn">
+            Login
+          </button>
         </form>
         <ToastContainer />
-        <p className='notMember'>Not a member? <NavLink className={'toSign'} to="/signup">Sign Up </NavLink> </p>
+        <p className="notMember">
+          Not a member?{" "}
+          <NavLink className={"toSign"} to="/signup">
+            Sign Up{" "}
+          </NavLink>{" "}
+        </p>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,20 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import axios from 'axios'
-import '../Login/Login.css'
-
+import React, { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import axios from "axios";
+import "../Login/Login.css";
 
 const Signup = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [data, setData] = useState({})
-  const [fetch, setFetch] = useState(false)
-  const navigate = useNavigate()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [data, setData] = useState({});
+  const [fetch, setFetch] = useState(false);
+  const [passColor, setPassColor] = useState("");
+  const [passVisible, setPassVisible] = useState(false);
+  const [passResetVisible, setPassResetVisible] = useState(false);
+  const [passResetColor, setPassResetColor] = useState("");
+  const navigate = useNavigate();
 
   const validateEmail = (email) => {
     return String(email)
@@ -25,94 +29,201 @@ const Signup = () => {
   };
 
   useEffect(() => {
-    axios.post('http://localhost:4000/user/signup', data)
+    if (password.length > 0) setPassColor("red");
+    if (password.length === 0) setPassColor("");
+    if (password.length > 8) setPassColor("green");
+  }, [password]);
+
+  useEffect(() => {
+    if (confirmPassword.length > 0) setPassResetColor("red");
+    if (confirmPassword.length === 0) setPassResetColor("");
+    if (confirmPassword.length > 8) setPassResetColor("green");
+  }, [confirmPassword]);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:4000/user/signup", data)
       .then(() => {
-        toast.success('Redirecting to login page..', { position: "top-right", autoClose: 500, hideProgressBar: true, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, theme: "light", });
+        toast.success("Redirecting to login page..", {
+          position: "top-right",
+          autoClose: 500,
+          hideProgressBar: true,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
         setTimeout(() => {
-          navigate('/login')
-        }, 1000)
-        })
-      .catch(err => {
-        if (err.response.data.message === "Email need unique") {
-          toast.error('This email was used', { position: "top-right", autoClose: 500, hideProgressBar: true, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, theme: "light", });
-        }
+          navigate("/login");
+        }, 1000);
       })
-  }, [fetch])
+      .catch((err) => {
+        if (err.response.data.message === "Email need unique") {
+          toast.error("This email was used", {
+            position: "top-right",
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        }
+      });
+  }, [fetch]);
 
   const submitForm = (e) => {
     e.preventDefault();
     if (!name || !email || !password || !confirmPassword) {
-      toast.error('Please enter all information', { position: "top-right", autoClose: 500, hideProgressBar: true, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, theme: "light", });
+      toast.error("Please enter all information", {
+        position: "top-right",
+        autoClose: 500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       return;
     }
 
-    const emailFormat = validateEmail(email)
+    const emailFormat = validateEmail(email);
     if (!emailFormat) {
-      toast.error('Email is not correct format', { position: "top-right", autoClose: 500, hideProgressBar: true, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, theme: "light", });
+      toast.error("Email is not correct format", {
+        position: "top-right",
+        autoClose: 500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+      return;
+    }
+
+    if (password < 8 || confirmPassword < 8) {
+      toast.error("Passwords must be more than 8", {
+        position: "top-right",
+        autoClose: 500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       return;
     }
 
     if (password !== confirmPassword) {
-      toast.error('Passwords are not same', { position: "top-right", autoClose: 500, hideProgressBar: true, closeOnClick: true, pauseOnHover: true, draggable: true, progress: undefined, theme: "light", });
+      toast.error("Passwords are not same", {
+        position: "top-right",
+        autoClose: 500,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       return;
     }
-    setData({ name, email, password, confirmPassword })
+    setData({ name, email, password, confirmPassword });
     if (password === confirmPassword) {
-      setFetch(!fetch)
+      setFetch(!fetch);
     }
-  }
+  };
+
   return (
-    <main className='loginContainer'>
+    <main className="loginContainer">
       <div className="login">
         <h2>Sign Up</h2>
         <form onSubmit={(e) => submitForm(e)}>
           <div className="input">
             <input
-              className='text'
+              className="text"
               type="text"
-              id='name'
+              id="name"
               value={name}
-              onChange={e => setName(e.target.value)}
-              placeholder=' ' />
+              onChange={(e) => setName(e.target.value)}
+              placeholder=" "
+            />
             <label htmlFor="name">Username</label>
           </div>
           <div className="input">
             <input
-              className='text'
+              className="text"
               type="text"
-              id='emailSign'
+              id="emailSign"
               value={email}
-              onChange={e => setEmail(e.target.value)}
-              placeholder=' ' />
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder=" "
+            />
             <label htmlFor="emailSign">Email</label>
           </div>
           <div className="input">
             <input
-              className='text'
-              type="password"
-              id='password'
+              style={
+                passColor === "red"
+                  ? { border: "1px solid red" }
+                  : passColor === "green"
+                  ? { border: "1px solid green" }
+                  : { border: "1px solid #ccc" }
+              }
+              className="text"
+              type={!passVisible ? "password" : "text"}
+              id="password"
               value={password}
-              onChange={e => setPassword(e.target.value)}
-              placeholder=' ' />
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder=" "
+            />
             <label htmlFor="password">Password</label>
+            <p className="icon" onClick={() => setPassVisible(!passVisible)}>
+              {!passVisible ? <FaEye /> : <FaEyeSlash />}
+            </p>
           </div>
           <div className="input">
             <input
-              className='text'
-              type="password"
-              id='passwordConfirm'
+              style={
+                passResetColor === "red"
+                  ? { border: "1px solid red" }
+                  : passResetColor === "green"
+                  ? { border: "1px solid green" }
+                  : { border: "1px solid #ccc" }
+              }
+              className="text"
+              type={!passVisible ? "password" : "text"}
+              id="passwordConfirm"
               value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-              placeholder=' ' />
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              placeholder=" "
+            />
             <label htmlFor="passwordConfirm">Confirm password</label>
+            <p
+              className="icon"
+              onClick={() => setPassResetVisible(!passResetVisible)}
+            >
+              {!passResetVisible ? <FaEye /> : <FaEyeSlash />}
+            </p>
           </div>
-          <button type="submit" className='submitBtn'>Sign Up</button>
+          <button type="submit" className="submitBtn">
+            Sign Up
+          </button>
         </form>
-        <ToastContainer/>
-        <p className='notMember'>Already member? <NavLink className={'toSign'} to="/login">Login</NavLink> </p>
+        <ToastContainer />
+        <p className="notMember">
+          Already member?{" "}
+          <NavLink className={"toSign"} to="/login">
+            Login
+          </NavLink>{" "}
+        </p>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
