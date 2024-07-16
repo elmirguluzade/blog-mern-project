b0c57ec4b72c22690c5839525e84a382121d94d4
Elmir Guluzade <elmirguluzade@Elmirs-MacBook-Air.local>
2024-07-16 23:22:08 +0400

    file path updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import ContextProvider from './Context';
+import ContextProvider from './Context.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
